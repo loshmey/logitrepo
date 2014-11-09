d7a1c06f6b915ab369e2b2b03891735e03ed0939
Milos <loshmey@yahoo.com>
2014-11-10 00:45:56 +0100

    radi servis

--- src/main/java/com/lo/apps/ws/endpoint/InvoiceEndpoint.java
+++ src/main/java/com/lo/apps/ws/endpoint/InvoiceEndpoint.java
@@ -1,13 +1,15 @@
 package com.lo.apps.ws.endpoint;
 
-import org.jdom2.Element;
-import org.jdom2.xpath.XPathExpression;
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import org.jdom2.JDOMException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.ParseException;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapHeader;
 
 import com.lo.apps.ws.entity.invoice.InvoiceRequest;
 import com.lo.apps.ws.entity.invoice.InvoiceResponse;
@@ -27,28 +29,16 @@ public class InvoiceEndpoint {
 
 	private static final String NAMESPACE_URI = "http://localhost:8080/holidayService/";
 
-	// private XPathExpression<Element> invoiceRequest;
-	// private XPathExpression<Element> invoiceResponse;
-
 	@Autowired
 	private InvoiceRequestService invoiceService;
 
 	@Autowired
 	private InvoiceRepo repo;
 
-	// @Autowired
-	// public InvoiceEndpoint(InvoiceRequestService invoiceService) throws
-	// JDOMException {
-	// this.invoiceService = invoiceService;
-	//
-	// Namespace namespace = Namespace.getNamespace("ih", NAMESPACE_URI);
-	// XPathFactory factory = XPathFactory.instance();
-	//
-	// invoiceRequest = factory.compile("//ih:InvoiceRequest",
-	// Filters.element(), null, namespace);
-	// invoiceResponse = factory.compile("//ih:InvoiceResponse",
-	// Filters.element(), null, namespace);
-	// }
+	@Autowired
+	public InvoiceEndpoint(InvoiceRequestService invoiceService) throws JDOMException {
+		this.invoiceService = invoiceService;
+	}
 
 	/**
 	 * Handles invoice request and sends response back.
@@ -61,66 +51,15 @@ public class InvoiceEndpoint {
 	 */
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "InvoiceRequest")
 	@ResponsePayload
-	public InvoiceResponse handleInvoiceRequest(@RequestPayload InvoiceRequest invoiceRequestElement) throws Exception {
-		// InvoiceRequest invReq = parseRequestElement(invoiceRequest,
-		// invoiceRequestElement);
-		//
-		// InvoiceResponse invRes = parseResponseElement(invoiceResponse,
-		// invoiceRequestElement);
-
+	public JAXBElement<InvoiceResponse> handleInvoiceRequest(@RequestPayload JAXBElement<InvoiceRequest> invoiceRequestElement, SoapHeader soapHeader) throws Exception {
 		// TODO zameniti sa pravim posle testa
 		System.out.println(invoiceRequestElement);
 		InvoiceResponse invRes = new ObjectFactory().createInvoiceResponse();
-		// JAXBElement<InvoiceResponse> responseElement = new
-		// JAXBElement<InvoiceResponse>(new
-		// QName(InvoiceResponse.class.getSimpleName()), InvoiceResponse.class,
-		// invRes);
 		invRes.setDescription(repo.getInvoice().toString());
 		invRes.setStatus(Status.OK);
 
-		// responseElement.setValue(invRes);
+		JAXBElement<InvoiceResponse> invResJaxb = new JAXBElement<InvoiceResponse>(new QName(InvoiceResponse.class.getSimpleName()), InvoiceResponse.class, null, invRes);
 
-		return invRes;
+		return invResJaxb;
 	}
-
-	/**
-	 * Parse request element from schema.
-	 * 
-	 * @param expression
-	 * @param element
-	 * @return
-	 * @throws ParseException
-	 */
-	private InvoiceRequest parseRequestElement(XPathExpression<Element> expression, Element element) throws ParseException {
-		Element result = expression.evaluateFirst(element);
-		if (result != null) {
-			InvoiceRequest invReq = new InvoiceRequest();
-			result.getAttributes();
-			result.getContent();
-			return invReq;
-		} else {
-			throw new IllegalArgumentException("Could not evaluate [" + expression + "] on [" + element + "]");
-		}
-	}
-
-	/**
-	 * Parse response element from schema.
-	 * 
-	 * @param expression
-	 * @param element
-	 * @return
-	 * @throws ParseException
-	 */
-	private InvoiceResponse parseResponseElement(XPathExpression<Element> expression, Element element) throws ParseException {
-		Element result = expression.evaluateFirst(element);
-		if (result != null) {
-			InvoiceResponse invRes = new InvoiceResponse();
-			result.getAttributes();
-			result.getContent();
-			return invRes;
-		} else {
-			throw new IllegalArgumentException("Could not evaluate [" + expression + "] on [" + element + "]");
-		}
-	}
-
 }
